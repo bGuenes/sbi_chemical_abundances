ae33053cc4926a9fd65426c7823bba1b658e028e
bGuenes <84253749+bGuenes@users.noreply.github.com>
2024-08-28 01:55:47 +0200

    plot

--- plot_functions.py
+++ plot_functions.py
@@ -5,6 +5,16 @@ from scipy.stats import norm
 from scipy.stats import multivariate_normal
 
 ##########################################################################################################
+# --- Calculate mean and error ---
+def mean_error(x, N_stars):
+    mean = np.mean(x)
+    std = np.std(x)
+    err = std/np.sqrt(N_stars)
+
+    return mean, err, std
+
+
+##########################################################################################################
 # --- Plot 1D Histogram ---
 
 def plot_1d_hist(x1, x2, x_true, names, simulations):
@@ -12,27 +22,26 @@ def plot_1d_hist(x1, x2, x_true, names, simulations):
     plot_width = 0.3
     
     def plot_subplot(i, x, true, name):
-        mean = np.mean(x)
-        std = np.std(x)
+        N_stars = int(len(x)/simulations)
+        mean, err, std = mean_error(x, N_stars)
 
+        # Set the x-axis limits
         x_min = true - plot_width*true
         x_max = true + plot_width*true
 
+        # Fit a Gaussian to the data
         lin = np.linspace(x_min, x_max, 1000)
         p = norm.pdf(lin, mean, std)
 
-        # fit of mean
-        std_mu = std/np.sqrt(len(x)/simulations)
-        mu = norm.pdf(lin, mean, std_mu)
-
+        # Plot the data
         ax[i].hist(x, bins=500, density=True, alpha=0.6, color='g')
         ax[i].plot(lin, p, 'k', linewidth=2)
         ax[i].axvline(x=true, color='r', linestyle='dashed', linewidth=2)
+        
         ax[i].set_title(fr"{name}: {true:.3f}"
                         "\n"
-                        fr"$\mu$: {mean:.3f} $\sigma$: {std_mu:.3f}")
+                        fr"Fit: {mean:.3f} $\pm$ {err:.3f}")
         ax[i].set_xlim(x_min, x_max)
-        ax[i].plot(lin, p.max()*mu/mu.max(), color="orange")
 
     for i in [0,1]:
         if i == 0:
@@ -47,14 +56,17 @@ def plot_1d_hist(x1, x2, x_true, names, simulations):
     plt.show()
 
 
-##########################################################################################################
-# --- Plot 2D Histogram ---
 
-def plot_2d_hist(x1, x2, x_true, N_stars):
+##########################################################################################################
+# --- 2D Histogram preparation ---
+def hist_2d_prep(x1, x2, x_true, N_stars):
     x_lim, y_lim = [-2.8,-1.8],[-3.4,-2.4]
 
     # --- Fit a 2D Gaussian to the data ---
-    popt, pcov = multivariate_normal.fit(np.array([x1, x2]).T)
+    mean_1, err_1, std_1 = mean_error(x1, int(N_stars))
+    mean_2, err_2, std_2 = mean_error(x2, int(N_stars))
+
+    
     x = np.linspace(x_lim[0], x_lim[1], 1000)
     y = np.linspace(y_lim[0], y_lim[1], 1000)
 
@@ -64,16 +76,24 @@ def plot_2d_hist(x1, x2, x_true, N_stars):
     pos[:, :, 0] = X
     pos[:, :, 1] = Y
 
+    popt, pcov = multivariate_normal.fit(np.array([x1, x2]).T)
     rv = multivariate_normal(popt, pcov)
-    perr = np.sqrt(np.diag(pcov))
-    mu_std = perr / np.sqrt(N_stars)
+
 
     # --- Calculate the sigma levels ---
     levels =[]
     sigma = [3,2,1]
     for n in sigma:
-        level = rv.pdf(popt+n * np.array([perr[0], perr[1]]))
+        level = rv.pdf(popt+n * np.array([std_1, std_2]))
         levels.append(level)
+
+    return  x_lim, y_lim, X, Y, pos, rv, levels, sigma, mean_1, err_1, std_1, mean_2, err_2, std_2
+
+##########################################################################################################
+# --- Plot 2D Histogram ---
+
+def plot_2d_hist(x1, x2, x_true, N_stars):
+    x_lim, y_lim, X, Y, pos, rv, levels, sigma, mean_1, err_1, std_1, mean_2, err_2, std_2 = hist_2d_prep(x1, x2, x_true, N_stars)
 
     # --- Plot the data ---
     plt.figure(figsize=(15,15))
@@ -82,13 +102,11 @@ def plot_2d_hist(x1, x2, x_true, N_stars):
     # draw contour lines on1,2,3 sigma
     CS = plt.contour(X, Y, rv.pdf(pos), levels=levels, colors='k', linestyles='dashed')
     text = plt.clabel(CS, inline=True, fontsize=10)
-    c_labels = np.array(sigma)
-    for i in range(len(text)):
-        text[i].set(text=f'{c_labels[i]} $\\sigma$')
+    for i in range(len(sigma)):
+        text[i].set(text=f'{sigma[i]} $\\sigma$')
 
     plt.scatter(x_true[0], x_true[1], color='r', label='Ground Truth', s=10)
-    #plt.scatter(popt[0], popt[1], color='k', marker='x', label='Fit')
-    plt.errorbar(popt[0], popt[1], yerr=mu_std[1], xerr=mu_std[0], color='k', marker='.')
+    plt.errorbar(mean_1, mean_2, yerr=err_2, xerr=err_1, color='k', marker='.')
 
     plt.title(f'Posterior sampling of {N_stars} stars', fontsize=40)
 
@@ -102,29 +120,10 @@ def plot_2d_hist(x1, x2, x_true, N_stars):
 # --- Plot 2D Histogram with side plots ---
 
 def plot_2d_hist_sides(x1, x2, x_true, N_stars):
-    x_lim, y_lim = [-2.8,-1.8],[-3.4,-2.4]
+    x_lim, y_lim, X, Y, pos, rv, levels, sigma, mean_1, err_1, std_1, mean_2, err_2, std_2 = hist_2d_prep(x1, x2, x_true, N_stars)
 
-    # --- Fit a 2D Gaussian to the observed data ---
-    popt, pcov = multivariate_normal.fit(np.array([x1, x2]).T)
     x = np.linspace(x_lim[0], x_lim[1], 1000)
     y = np.linspace(y_lim[0], y_lim[1], 1000)
-
-    X, Y = np.meshgrid(x, y)
-    pos = np.empty(X.shape + (2,))
-
-    pos[:, :, 0] = X
-    pos[:, :, 1] = Y
-
-    rv = multivariate_normal(popt, pcov)
-    perr = np.sqrt(np.diag(pcov))
-    mu_std = perr / np.sqrt(N_stars)
-
-    # --- Calculate the sigma levels ---
-    levels =[]
-    sigma = [3,2,1]
-    for n in sigma:
-        level = rv.pdf(popt+n * np.array([perr[0], perr[1]]))
-        levels.append(level)
 
     # --- Plot the data ---
     fig = plt.figure(1, figsize=(15,15))
@@ -160,19 +159,17 @@ def plot_2d_hist_sides(x1, x2, x_true, N_stars):
     # draw contour lines on sigma levels
     CS = axTemperature.contour(X, Y, rv.pdf(pos), levels=levels, colors='k', linestyles='dashed')
     text = axTemperature.clabel(CS, inline=True, fontsize=10)
-    c_labels = np.array(sigma)
-    for i in range(len(c_labels)):
-        text[i].set(text=f'{c_labels[i]} $\\sigma$')
+    for i in range(len(sigma)):
+        text[i].set(text=f'{sigma[i]} $\\sigma$')
 
     # labels
     label_gt = r'Ground Truth' + f"\n" + r"$\alpha_{\rm IMF} = $" + f'${round(x_true[0].item(), 2)}$' + f"\n" + r"$\log_{10} N_{\rm Ia} = $" + f'${round(x_true[1].item(), 2)}$'
     
-    label_fit = r'Fit' + f"\n" + r"$\alpha_{\rm IMF} = $" + f'${round(popt[0].item(), 2)} \\pm {round(mu_std[0].item(),2)}$' + f"\n" + r"$\log_{10} N_{\rm Ia} = $" + f'${round(popt[1].item(), 2)} \\pm {round(mu_std[1].item(),2)}$'
+    label_fit = r'Fit' + f"\n" + r"$\alpha_{\rm IMF} = $" + f'${round(mean_1.item(), 3)} \\pm {round(err_1.item(),3)}$' + f"\n" + r"$\log_{10} N_{\rm Ia} = $" + f'${round(mean_2.item(), 3)} \\pm {round(err_2.item(),3)}$'
     
     # plot the ground truth and the fit
     legend_true = axTemperature.scatter(x_true[0], x_true[1], color='r', label=label_gt, s=10)
-    #legend_fit = axTemperature.scatter(popt[0], popt[1], color='k', marker='x', label=label_fit)
-    legend_fit = axTemperature.errorbar(popt[0], popt[1], yerr=mu_std[1], xerr=mu_std[0], color='k', marker='.', label=label_fit)
+    legend_fit = axTemperature.errorbar(mean_1, mean_2, yerr=err_2, xerr=err_1, color='k', marker='.', label=label_fit)
 
     axTemperature.set_xlabel(r'$\alpha_{\rm IMF}$', fontsize=20)
     axTemperature.set_ylabel(r'$\log_{10} N_{\rm Ia}$', fontsize=20)
@@ -182,21 +179,18 @@ def plot_2d_hist_sides(x1, x2, x_true, N_stars):
     axHistx.hist(x1, bins=500, density=True, alpha=0.6, color='g')
     axHisty.hist(x2, bins=500, density=True, alpha=0.6, color='g', orientation='horizontal')
 
-    axHistx.plot(x, norm.pdf(x, popt[0], perr[0]), 'k', linewidth=2)
-    axHisty.plot(norm.pdf(y, popt[1], perr[1]), y, 'k', linewidth=2)
+    axHistx.plot(x, norm.pdf(x, mean_1, std_1), 'k', linewidth=2)
+    axHisty.plot(norm.pdf(y, mean_2, std_2), y, 'k', linewidth=2)
 
     axHistx.axvline(x=x_true[0], color='r', linestyle='dashed', linewidth=2)
     axHisty.axhline(y=x_true[1], color='r', linestyle='dashed', linewidth=2)
 
     axHistx.set_xlim(x_lim)
-    axHisty.set_ylim(y_lim)
-
-    #fig.text(0.85, 0.95, r'$\alpha_{\rm IMF} = $'+f'${x_true[0]}$', fontsize=20)
-    #fig.text(0.85, 0.9, r'$\log_{10} N_{\rm Ia}$', fontsize=20)
-    
+    axHisty.set_ylim(y_lim)    
 
     fig.legend(handles=[legend_true], fontsize=15, shadow=True, fancybox=True, loc=2, bbox_to_anchor=(0.05, 0.92))
     fig.legend(handles=[legend_fit], fontsize=15, shadow=True, fancybox=True, loc=2, bbox_to_anchor=(0.05, 0.99))
+
     plt.show()
 
 
@@ -210,15 +204,12 @@ def n_stars_plot(x1, x2, x_true, no_stars= np.array([1, 10, 100, 500, 1000]), si
     # --- Fit a 2D Gaussian to the data ---
     for n in no_stars:
         samples = int(n*simulations)
-        popt, pcov = multivariate_normal.fit(np.array([x1[0:samples], x2[0:samples]]).T)
+        N_stars = int(samples/simulations)
+        mean_1, err_1, _ = mean_error(x1[0:samples], N_stars)
+        mean_2, err_2, _ = mean_error(x2[0:samples], N_stars)
 
-        mu1 = popt[0]
-        mu2 = popt[1]
-        sigma1 = np.sqrt(pcov[0,0]) / np.sqrt(n)
-        sigma2 = np.sqrt(pcov[1,1]) / np.sqrt(n)
-
-        fit.append([mu1, mu2])
-        err.append([sigma1, sigma2])
+        fit.append([mean_1, mean_2])
+        err.append([err_1, err_2])
         
 
     fit = np.array(fit)
@@ -227,34 +218,25 @@ def n_stars_plot(x1, x2, x_true, no_stars= np.array([1, 10, 100, 500, 1000]), si
     # --- Plot the data ---
     fig,ax=plt.subplots(nrows=1,ncols=2,figsize=(26,6))
 
-    # Plot alpha_IMF
-    ax[0].plot(no_stars, fit[:,0], color="b", label="Fit")
-    ax[0].fill_between(no_stars, fit[:,0]-err[:,0], fit[:,0]+err[:,0], alpha=0.3,color="b", label=r"1 & 2 $\sigma$")
-    ax[0].fill_between(no_stars, fit[:,0]-2*err[:,0], fit[:,0]+2*err[:,0], alpha=0.2,color="b")
+    def plot(fit, err, x_true, ax, name):
+        ax.plot(no_stars, fit, color="b", label="Fit")
+        ax.fill_between(no_stars, fit-err, fit+err, alpha=0.3,color="b", label=r"1 & 2 $\sigma$")
+        ax.fill_between(no_stars, fit-2*err, fit+2*err, alpha=0.2,color="b")
 
-    ax[0].axhline(x_true[0], color='k', linestyle=':', linewidth=2, label='Ground Truth')
+        ax.axhline(x_true, color='k', linestyle=':', linewidth=2, label='Ground Truth')
 
-    ax[0].set_xlabel(r'$N_{\rm stars}$', fontsize=20)
-    ax[0].set_ylabel(r'$\alpha_{\rm IMF}$', fontsize=20)
-    ax[0].set_ylim([-2.5,-2.1])
-    ax[0].set_xlim([1,1000])
-    ax[0].set_xscale('log')
+        ax.set_xlabel(r'$N_{\rm stars}$', fontsize=20)
+        ax.set_ylabel(name, fontsize=20)
+        ax.set_ylim([x_true-0.2*abs(x_true), x_true+0.2*abs(x_true)])
+        ax.set_xscale('log')
+        ax.set_xlim([1,1000])
+
+    for i, name in enumerate([r'$\alpha_{\rm IMF}$', r'$\log_{10} N_{\rm Ia}$']):
+        plot(fit[:,i], err[:,i], x_true[i], ax[i], name)
+
     ax[0].legend(fontsize=15, fancybox=True, shadow=True)
-
-    # Plot log10_N_Ia
-    ax[1].plot(no_stars, fit[:,1], color="b", label="Fit")
-    ax[1].fill_between(no_stars, fit[:,1]-err[:,1], fit[:,1]+err[:,1], alpha=0.3,color="b", label=r"1 & 2 $\sigma$")
-    ax[1].fill_between(no_stars, fit[:,1]-2*err[:,1], fit[:,1]+2*err[:,1], alpha=0.2,color="b")
-
-    ax[1].axhline(x_true[1], color='k', linestyle=':', linewidth=2, label='Ground Truth')
-
-    ax[1].set_xlabel(r'$N_{\rm stars}$', fontsize=20)
-    ax[1].set_ylabel(r'$\log_{10} N_{\rm Ia}$', fontsize=20)
-    ax[1].set_ylim([-3.05,-2.65])
-    ax[1].set_xlim([1,1000])
-    ax[1].set_xscale('log')
-    #ax[1].legend(fontsize=15, fancybox=True, shadow=True)
-
+    
+    plt.tight_layout()
     plt.show()
 
 
